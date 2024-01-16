fbd6786f5cb411fdb990065523b31fd0c926bf1a
nicolaspenailillo <45469802+nicolaspenailillo@users.noreply.github.com>
2024-01-16 09:50:44 +0100

    Update vaultMigration.py

--- vaultMigration.py
+++ vaultMigration.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
     if len(sys.argv) < 4 or len(sys.argv) > 6:
-        print("Usage: python vaultMigration.py old_vault_url new_vault_url secret_engine_to_migrate [optional_new_path_prefix] [optional_new_engine_name]")
+        print("Usage: python vaultMigration.py old_vault_url new_vault_url secret_engine_to_migrate [optional_new_engine_name] [optional_new_path_prefix]")
         sys.exit(1)
         
     # Get command line arguments
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     new_vault_url = sys.argv[2]
     secret_engine = sys.argv[3]
 
-    optional_new_path_prefix = sys.argv[4] if len(sys.argv) > 4 else None
-    optional_new_engine_name = sys.argv[5] if len(sys.argv) > 5 else None
+    optional_new_engine_name = sys.argv[4] if len(sys.argv) > 4 else None
+    optional_new_path_prefix = sys.argv[5] if len(sys.argv) > 5 else None
 
     # Connect old vault
     vault_old = VaultOIDCHandler(old_vault_url, role='')
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         # Do something
         pass
 
-    migrate_engine(vault_old.client, vault_new.client, secret_engine, optional_new_path_prefix, optional_new_engine_name)
+    migrate_engine(vault_old.client, vault_new.client, secret_engine, new_path_prefix=optional_new_path_prefix, new_engine_name=optional_new_engine_name)
